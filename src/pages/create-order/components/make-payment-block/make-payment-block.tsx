07ef137d39ea6416e0634e73dfab5e331d30c7a2
Ruban Daniil <danya.ruban.2005@mail.ru>
2025-03-21 18:13:32 +0300

    feat: Изменил отображение ошибки #DEV-0

--- src/pages/create-order/components/make-payment-block/make-payment-block.tsx
+++ src/pages/create-order/components/make-payment-block/make-payment-block.tsx
@@ -1,8 +1,8 @@
 import { useTranslation } from 'react-i18next';
 import { useSelector } from 'react-redux';
-import { RootState } from '@store/store';
-import { Alert, Button, Card, Flex, Typography } from 'antd';
 import { ChoppShadowCard } from '@shared/index';
+import { RootState } from '@store/store';
+import { Button, Flex, message, Typography } from 'antd';
 
 const { Title } = Typography;
 
@@ -13,25 +13,39 @@ type Props = {
 export const MakePaymentBlock = ({ error }: Props) => {
   const { shoppingCart } = useSelector((state: RootState) => state.shoppingCart);
   const { t } = useTranslation();
+  const [messageApi, contextHolder] = message.useMessage();
+
+  const err = () => {
+    messageApi.open({
+      type: 'error',
+      content: error,
+    });
+  };
 
   return (
-    <ChoppShadowCard className="md:w-1/4 h-36">
-      <Flex vertical gap={12}>
-        <Flex className="flex-row md:flex-col justify-between align-center" gap={24}>
-          <Flex gap={12} className="md:justify-between items-center">
-            <Title level={5} type="secondary" className="!m-0">
-              {t('IN_ALL')}
-            </Title>
-            <Title level={5} className="!m-0">
-              {shoppingCart.totalPrice}₽
-            </Title>
+    <>
+      {contextHolder}
+      <ChoppShadowCard className="md:w-1/4 h-36">
+        <Flex vertical gap={12}>
+          <Flex className="flex-row md:flex-col justify-between align-center" gap={24}>
+            <Flex gap={12} className="md:justify-between items-center">
+              <Title level={5} type="secondary" className="!m-0">
+                {t('IN_ALL')}
+              </Title>
+              <Title level={5} className="!m-0">
+                {shoppingCart.totalPrice}₽
+              </Title>
+            </Flex>
+            <Button
+              onClick={() => error !== undefined && err()}
+              htmlType="submit"
+              type="primary"
+              size="large">
+              {t('MAKE_PAYMENT')}
+            </Button>
           </Flex>
-          <Button htmlType="submit" type="primary" size="large">
-            {t('MAKE_PAYMENT')}
-          </Button>
         </Flex>
-        {error !== undefined && <Alert description={error} type="error" />}
-      </Flex>
-    </ChoppShadowCard>
+      </ChoppShadowCard>
+    </>
   );
 };
