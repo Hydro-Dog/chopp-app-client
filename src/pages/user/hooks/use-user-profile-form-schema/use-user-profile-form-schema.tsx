9f24d131e4f1026483c9a335c3787970dbda6749
Ruban Daniil <danya.ruban.2005@mail.ru>
2025-03-27 14:13:46 +0300

    feat: Отладка номера телефона#DEV-0

--- src/pages/user/hooks/use-user-profile-form-schema/use-user-profile-form-schema.tsx
+++ src/pages/user/hooks/use-user-profile-form-schema/use-user-profile-form-schema.tsx
@@ -4,7 +4,10 @@ import { z } from 'zod';
 export const useUserProfileFormSchema = () => {
   const { t } = useTranslation();
   return z.object({
-    phoneNumber: z.string().min(1, `${t('FORM_ERRORS.REQUIRED_FIELD')}`),
+    phoneNumber: z
+      .string()
+      .min(1, `${t('FORM_ERRORS.REQUIRED_FIELD')}`)
+      .regex(/^\+7 \(\d{3}\) \d{3}-\d{2}-\d{2}$/, `${t('FORM_ERRORS.INVALID_PHONE_NUMBER')}`),
     fullName: z
       .string()
       .min(8, `${t('FORM_ERRORS.MIN_LENGTH', { count: 8 })}`)
