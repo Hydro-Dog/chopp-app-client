886484fe6ea8e4b31c40d6ee8b638a8c8d953069
Ruban Daniil <danya.ruban.2005@mail.ru>
2025-04-12 18:03:06 +0300

    feat: правки #DEV-0

--- src/pages/user/user-page.tsx
+++ src/pages/user/user-page.tsx
@@ -1,45 +1,44 @@
-import { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useDispatch, useSelector } from 'react-redux';
 import { HomeOutlined } from '@ant-design/icons';
 import { ChoppShadowCard, ChoppSubPage } from '@shared/components';
 import { useNotificationContext } from '@shared/context';
 import { STORAGE_KEYS } from '@shared/enum';
+import { useSuperDispatch } from '@shared/hooks';
 import { FETCH_STATUS } from '@shared/types';
 import { logout, setLogoutStatus } from '@store/slices';
 import { AppDispatch, RootState } from '@store/store';
-import { useRootContext } from '@widgets/root-container/root-provider';
-import { Button, Flex } from 'antd';
+import { Button, Flex, Typography } from 'antd';
 import { UserProfileForm } from './components';
+const { Title } = Typography;
+
 export const UserPage = () => {
   const { t } = useTranslation();
   const dispatch = useDispatch<AppDispatch>();
   const { logoutStatus } = useSelector((state: RootState) => state.user);
   const { showNotification } = useNotificationContext();
-  const { openLoginModal } = useRootContext();
+  const superDispatch = useSuperDispatch();
 
   const onLogout = () => {
-    dispatch(logout({ refreshToken: String(localStorage.getItem(STORAGE_KEYS.REFRESH_TOKEN)) }));
+    superDispatch.superDispatch({
+      action: logout({ refreshToken: String(localStorage.getItem(STORAGE_KEYS.REFRESH_TOKEN)) }),
+      thenHandler: () => {
+        if (logoutStatus === FETCH_STATUS.ERROR) {
+          showNotification({ type: 'error', message: 'Ошибка', description: 'Неудачный логаут' });
+        } else if (logoutStatus === FETCH_STATUS.SUCCESS) {
+          dispatch(setLogoutStatus(FETCH_STATUS.IDLE));
+        }
+      },
+    });
   };
-  useEffect(() => {
-    if (logoutStatus === FETCH_STATUS.ERROR) {
-      showNotification({ type: 'error', message: 'Ошибка', description: 'Неудачный логаут' });
-    } else if (logoutStatus === FETCH_STATUS.SUCCESS) {
-      dispatch(setLogoutStatus(FETCH_STATUS.IDLE));
-    }
-  }, [dispatch, logoutStatus, showNotification, openLoginModal]);
-
-  useEffect(() => {
-    if (!localStorage.getItem(STORAGE_KEYS.ACCESS_TOKEN)) {
-      openLoginModal();
-    }
-  }, []);
 
   return (
     <ChoppSubPage
       title={
         <Flex className="justify-between items-center w-full">
-          <div>{t('PROFILE')}</div>
+          <Title level={3} className="!m-0 !font-bold">
+            {t('PROFILE')}
+          </Title>
           <Button onClick={onLogout} danger size="large">
             {t('EXIT')}
           </Button>
