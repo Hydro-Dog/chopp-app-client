d9c34901cb087c383df2152dd9604b7e6337253b
Senpai <109900504+DaniiL10062005@users.noreply.github.com>
2025-05-13 12:21:12 +0300

    fix: отображение пустой корзины #DEV-86
    * fix: Логика отображения пустой корзины #DEV-86
    
    * fix: Переименовка ветки #DEV-86

--- src/pages/shopping-cart/shopping-cart-page.tsx
+++ src/pages/shopping-cart/shopping-cart-page.tsx
@@ -1,12 +1,16 @@
+import { useSelector } from 'react-redux';
 import { ChoppSubPage } from '@shared/index';
+import { RootState } from '@store/store';
 import { Flex } from 'antd';
-import { CreateOrderBlock, OrderItemsList } from './components';
+import { CreateOrderBlock, EmptyShoppingCart, OrderItemsList } from './components';
 
 const ShoppingCartPage = () => {
+  const { shoppingCart } = useSelector((state: RootState) => state.shoppingCart);
+
   return (
     <ChoppSubPage title="Корзина">
       <Flex gap={32} className="flex-col-reverse md:flex-row">
-        <OrderItemsList />
+        {shoppingCart.items.length !== 0 ? <OrderItemsList /> : <EmptyShoppingCart />}
         <CreateOrderBlock />
       </Flex>
     </ChoppSubPage>
